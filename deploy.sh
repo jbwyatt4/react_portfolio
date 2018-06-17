a6bd5abeb5dcd15f54de9aaeac2c7c6ce18265ff
John Wyatt <jbwyatt4@gmail.com>
2018-06-16 19:47:50 -0700

    -u for git push was not correct

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ git submodule update --remote --merge
 # local only command-you know if someone is plagiarizing this repo if they don't have this wcommand
 wcommit "Automated submodule update commit"
 git push
-git push -u github_mirror master 
+git push github_mirror master 
