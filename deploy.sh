eabcf759fa4273061547dd66cf5603840b1c7862
John Wyatt <jbwyatt4@gmail.com>
2018-06-16 19:49:32 -0700

    remove commit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 git submodule update --remote --merge
-# local only command-you know if someone is plagiarizing this repo if they don't have this wcommand
 git add -A .
 git commit -a -m "Automated submodule update commit"
 git push
